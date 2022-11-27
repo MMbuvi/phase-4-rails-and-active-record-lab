e3a7fb39dcfd30241a73bbf3b7c52c6898b7f36c
Meshack <mbuvi.meshack99@gmail.com>
2022-11-27 23:04:05 +0300

    commit tree 324d8b6f620d7785cf125fae16197056c19e07ac

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Mbuvi", last_name: "Meshack", grade: 87)
+Student.create!(first_name: "Mutisya", last_name: "Jackson", grade: 78)
+Student.create!(first_name: "Samuel", last_name: "Fred", grade: 77)
+Student.create!(first_name: "Benjamin", last_name: "Kelvin", grade: 58)
+# Student.create!(first_name: "Richard, last_name:"Kamayu", grade: 81)
+
+
+
+
+
+
+
+
+
+
+# t.string :first_name
+# t.string :last_name
+# t.integer :grade
